76b1533c6c60d365d70f0782ece394cf1edf3498
Pedro Sobral <pedrosobral666.uk@gmail.com>
2022-04-23 20:58:00 +0100

    Add native libraries and plugins

--- screens/NotFoundScreen.tsx
+++ screens/NotFoundScreen.tsx
@@ -2,35 +2,45 @@ import { StyleSheet, TouchableOpacity } from 'react-native';
 
 import { Text, View } from '../components/Themed';
 import { RootStackScreenProps } from '../types';
+import styled from 'styled-components/native';
 
-export default function NotFoundScreen({ navigation }: RootStackScreenProps<'NotFound'>) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>This screen doesn't exist.</Text>
-      <TouchableOpacity onPress={() => navigation.replace('Root')} style={styles.link}>
-        <Text style={styles.linkText}>Go to home screen!</Text>
-      </TouchableOpacity>
-    </View>
-  );
+const Hello = styled;
+
+export default function NotFoundScreen({
+	navigation,
+}: RootStackScreenProps<'NotFound'>) {
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>This screen doesn't exist.</Text>
+			<TouchableOpacity
+				onPress={() => navigation.replace('Root')}
+				style={styles.link}
+			>
+				<Text style={styles.linkText}>Go to home screen!</Text>
+			</TouchableOpacity>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-  linkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		padding: 20,
+		backgroundColor: 'purple',
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: 'bold',
+		fontFamily: 'space-mono',
+	},
+	link: {
+		marginTop: 15,
+		paddingVertical: 15,
+	},
+	linkText: {
+		fontSize: 14,
+		color: '#2e78b7',
+	},
 });
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,52 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
+import ImageCropPicker from 'react-native-image-crop-picker';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import ImagePicker from 'react-native-image-crop-picker';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
+const handleImage = () => {
+	ImagePicker.openPicker({
+		width: 300,
+		height: 400,
+		cropping: true,
+		cropperCircleOverlay: true,
+	}).then(image => {
+		console.log(image);
+	});
+};
+
+export default function TabOneScreen({
+	navigation,
+}: RootTabScreenProps<'TabOne'>) {
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Tab One</Text>
+			<View
+				style={styles.separator}
+				lightColor="#eee"
+				darkColor="rgba(255,255,255,0.1)"
+			/>
+			<Button title="gggg" onPress={handleImage} />
+			<EditScreenInfo path="/screens/TabOneScreen.tsx" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: 'bold',
+	},
+	separator: {
+		marginVertical: 30,
+		height: 1,
+		width: '80%',
+	},
 });
